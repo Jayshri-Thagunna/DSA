1ba9b486bf8bed50929aae0760c543f02583bf38
Jayshri-Thagunna <thagunnajayshri02@gmail.com>
2026-03-11 07:35:08 +0530

    All Stack Operations

---
+++ AllStackOperation.cpp
@@ -0,0 +1,78 @@
+#include<iostream>
+using namespace std;
+
+//using LinkedList
+
+class Node{
+    public:
+        int data;
+        Node *next;
+};
+
+class Stack{
+    Node * head;
+
+    public:
+        Stack() {
+            head = NULL;
+        }
+
+void display() {
+    if(head == NULL){
+        cout << "Stack is Empty" << endl;
+    } else {
+        Node * temp = head;
+        while(temp != NULL){
+            cout << temp -> data << " ";
+            temp = temp -> next;
+        }
+        cout << endl;
+        }
+    }
+
+    //Push
+void Push(int value){
+    Node * newNode = new Node();
+    newNode -> data = value;
+    newNode -> next = head;
+    head = newNode;
+}
+
+    //Pop
+void Pop(){
+    if(head == NULL){
+        cout << "Stack Overflow" << endl;
+    } else {
+        Node *temp = head;
+        head = head -> next;
+        delete temp;
+    }
+}
+
+    //Peek
+void peek(){
+    if(head == NULL){
+        cout << "Stack is empty" << endl;
+    } else {
+        cout << " Top element: " << head -> data << endl;
+    }
+}
+
+};
+
+int main() {
+    Stack s;
+
+    s.display();
+
+    s.Push(4);
+    s.Push(5);
+    s.Push(6);
+    s.display();
+
+    s.Pop();
+    s.display();
+
+    s.peek();
+    s.display();
+}
--- main.cpp
+++ main.cpp
@@ -3,3 +3,59 @@
 using namespace std;
 
 // snode, dnode, sdisp, ddisp, shead, dhead
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int data) {
+        this -> data = data;
+        this -> next = NULL;
+    }
+};
+
+class Node {
+public:
+    int data;
+    Node* prev;
+    Node* next;
+
+    Node(int data) {
+        this -> data = data;
+        this -> prev = NULL;
+        this -> next = NULL;
+    }
+};
+
+void display(Node* head) {
+    Node* temp = head;
+    while (temp != NULL) {
+        cout << temp -> data << " -> ";
+        temp = temp -> next;
+    }
+    cout << "NULL" << endl;
+}
+
+void display(Node* head) {
+    Node* temp = head;
+    while (temp != NULL) {
+        cout << temp -> data << " <-> ";
+        temp = temp -> next;
+    }
+    cout << "NULL" << endl;
+}
+
+void insertAtHead(Node* &head, int d) {
+    Node* temp = new Node(d);
+    temp -> next = head;
+    head = temp;
+}
+
+void insertAtHead(Node* &head, int d) {
+    Node* temp = new Node(d);
+    if (head != NULL) {
+        head -> prev = temp;
+    }
+    temp -> next = head;
+    head = temp;
+}
